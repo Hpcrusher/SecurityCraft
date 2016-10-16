227d8b5144c05639bd05ee1787c945f3e86926a9
Hpcrusher <david-liebl@onlinehome.de>
2016-10-16 13:58:42 +0200

    More Setup

---
+++ src/main/java/com/hpcrusher/securitycraft/SecurityCraft.java
@@ -0,0 +1,29 @@
+package com.hpcrusher.securitycraft;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = SecurityCraft.MODID, version = SecurityCraft.VERSION)
+public class SecurityCraft {
+
+    public static final String MODID = "SecurityCraft";
+    public static final String VERSION = "1.10.2-0.1";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
